170ed18b6dbd7669d5f759e72c258542194434f9
chris <ctausendfreund@gmx.de>
2018-12-05 19:58:56 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Stage 1
+FROM maven:3-jdk-11-slim as builder
+
+WORKDIR /myapp
+COPY pom.xml .
+COPY src/ ./src/
+
+RUN mvn package
+
+# Stage 2
+FROM openjdk:11-jre-slim
+
+WORKDIR /myapp
+COPY --from=builder /myapp/target/*.jar ./app.jar
+
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "./app.jar"]
---
+++ src/main/java/com/example/springbootdockerhelloworld/SpringBootDockerHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootdockerhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerHelloWorldApplication.class, args);
+    }
+}
